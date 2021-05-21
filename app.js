faebcdffaeb7a160cabecbe15ae118eac8146ad9
niramay447 <54571224+niramay447@users.noreply.github.com>
2021-05-21 16:56:12 +0530

    new commit

---
+++ app.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+var items = []; 
+var workItems = [];
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+
+app.get("/", function (req, res) {
+  var today = new Date();
+
+  var options = {
+    weekday:"long",
+    day:"numeric",
+    month:"long"
+  };
+  
+  var day = today.toLocaleDateString("en-US",options);
+  
+  res.render("list", {
+    ListTitle: day,
+    newListItems: items
+  });
+});
+
+app.get("/work",function(req,res){
+  res.render("list", {
+    ListTitle: "Work List",
+    newListItems: workItems
+  });
+});
+
+app.post("/work",function(req,res){
+  let item = req.body.newItem;
+  workItems.push(item);
+  res.redirect("/work")
+});
+
+
+app.post("/", function(req,res){
+  var item = req.body.newItem;
+  if (req.body.list==="Work"){
+    workItems.push(item);
+    res.redirect("/work");
+  }else{
+    items.push(item);
+    res.redirect("/");
+  }
+});
+app.listen(3000, function () {
+  console.log("server started on port 3000");
+});
